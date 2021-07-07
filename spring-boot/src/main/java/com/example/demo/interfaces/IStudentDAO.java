4963b473e7d1cf8d8c7cdc2a8b7fee05c94535ef
WasimJafar <wjafar0002@gmail.com>
2021-07-07 14:31:09 +0530

    Checkpoint : 2:30

---
+++ spring-boot/src/main/java/com/example/demo/interfaces/IStudentDAO.java
@@ -0,0 +1,25 @@
+package com.example.demo.interfaces;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Student;
+
+//DT: POJO, PK DT
+@Service
+public interface IStudentDAO extends JpaRepository<Student, String> {
+	
+	public List<Student> findByFname(String fname);
+	
+	public List<Student> findByFnameAndLname(String fname, String lname);
+	
+	@Query(value="SELECT * FROM student a NATURAL JOIN marks b WHERE a.id = b.id", nativeQuery = true)
+	public List<String> selectJoinStudentAndMarks();
+	
+	@Query(value="SELECT * FROM student WHERE fname = ?1", nativeQuery = true)
+	public List<Student> getStudentByNameCustum(String fnamess);
+	
+}
--- spring-mvc/src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-
-<html>  
-<body>  
-<a href="hello">Click here...</a>  
-</body>  
-</html>  
-
-
