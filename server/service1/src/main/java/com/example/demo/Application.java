e89ccc57178c6717cdfe41006f6a428ae1343b3a
WasimJafar <wjafar0002@gmail.com>
2021-07-10 21:08:20 +0530

    EOD

--- server/service1/src/main/java/com/example/demo/Application.java
+++ server/service1/src/main/java/com/example/demo/Application.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
--- server/service2/src/main/java/com/sapient/SpringBootMongoApplication.java
+++ server/service2/src/main/java/com/sapient/SpringBootMongoApplication.java
@@ -2,8 +2,13 @@ package com.sapient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class SpringBootMongoApplication {
 
 	public static void main(String[] args) {
